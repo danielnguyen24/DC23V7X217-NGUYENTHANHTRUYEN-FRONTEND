9a9070bb7f5b7151faf95b43a7a6776afdc1ae5e
danielnguyen24 <truyendc23v7x217@dttx.ctu.edu.vn>
2024-11-24 18:10:14 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar navbar-expand navbar-dark bg-dark">
+    <a href="/" class="navbar-brand">Ứng dụng Quản lý danh bạ</a>
+    <div class="mr-auto navbar-nav">
+      <li class="nav-item">
+        <router-link :to="{ name: 'contactbook' }" class="nav-link">
+          Danh bạ
+          <i class="fas fa-address-book"></i>
+        </router-link>
+      </li>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<style scoped>
+.navbar {
+  margin-bottom: 20px;
+}
+</style>
